57111b031312a975bdf8e730c089fc0840aaadc1
TWEI <50558106+TWEI-0@users.noreply.github.com>
2019-06-02 19:34:01 +0900

    refactoring

--- wordpress/automation_sitemap/page_sitemap.php
+++ wordpress/automation_sitemap/page_sitemap.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Sitemap.
+Template Name: Sitemap
 Template Post Type: page
 */
 ?>
@@ -10,35 +10,43 @@ Template Post Type: page
 		<li><h2><a href="<?php echo home_url(); ?>">HOME</a><h2></li>
 
 <?php
-		$get_categories_args = array("parent" => 0);
-		$categories = get_categories($get_categories_args);
-		foreach($categories as $category) show_child_category($category, 1);
-		function show_child_category($parent_category, $depth) {
-			$category_link = get_category_link($parent_category->cat_ID);
-			echo "<li><h2><a href='{$category_link}'>{$parent_category->name}</a></h2></li>";
-			echo "<p>{$parent_category->description}</p>";
-			echo "<ul>";
+		$get_categories_args = array(
+			'parent' => 0,
+		);
+		$categories = get_categories( $get_categories_args );
+		foreach( $categories as $category ) {
+			show_child_category( $category, 1 );
+		}
+		function show_child_category( $parent_category, $depth ) {
+			$category_link = get_category_link( $parent_category->cat_ID );
+			echo "<li><h2><a href='$category_link'>$parent_category->name</a></h2></li>";
+			echo "<p>$parent_category->description</p>";
+			echo '<ul>';
 			global $post;
-			$posts = get_posts("numberposts=-1&category=" . $parent_category->cat_ID);
-			foreach($posts as $post) {
+			$posts = get_posts( 'numberposts=-1&category=' . $parent_category->cat_ID );
+			foreach( $posts as $post ) {
 				setup_postdata($post);
-				$post_category = get_the_category($post->ID);
-				if (count($post_category) > $depth) continue;
+				$post_category = get_the_category( $post->ID );
+				if ( count( $post_category ) > $depth ) {
+					continue;
+				}
 ?>
 				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 <?php
 			}
-			$get_categories_args = array("parent" => $parent_category->cat_ID);
-			$categories = get_categories($get_categories_args);
-			foreach($categories as $category) show_child_category($category, $depth + 1);
-			echo "</ul>";
+			$get_categories_args = array(
+				'parent' => $parent_category->cat_ID,
+			);
+			$categories = get_categories( $get_categories_args );
+			foreach( $categories as $category ) show_child_category( $category, $depth + 1 );
+			echo '</ul>';
 		}
 ?>
 
 		<li><h2>固定ページ</h2></li>
 		<ul>
 <?php
-			wp_list_pages("title_li=");
+			wp_list_pages( 'title_li=' );
 ?>
 		</ul>
 	</ul>
